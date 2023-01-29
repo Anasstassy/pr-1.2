0b4638faa13707e2bba304b33fd24f189fd81078
rytsa <rytsa@LAPTOP-0NIFEMSI>
2023-01-29 20:17:32 +0600

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace pr_1._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("SimSun", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(196, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "считать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.MistyRose;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox1.Location = new System.Drawing.Point(39, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(262, 25);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "x =";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.MistyRose;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox2.Location = new System.Drawing.Point(39, 92);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(262, 23);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = " b =";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.BackColor = System.Drawing.Color.RosyBrown;
+            this.checkBox1.Font = new System.Drawing.Font("Yu Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox1.Location = new System.Drawing.Point(361, 41);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(104, 24);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "2 * x";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.BackColor = System.Drawing.Color.RosyBrown;
+            this.checkBox2.Font = new System.Drawing.Font("Yu Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkBox2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox2.Location = new System.Drawing.Point(361, 71);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(104, 24);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "x + 4";
+            this.checkBox2.UseVisualStyleBackColor = false;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.BackColor = System.Drawing.Color.RosyBrown;
+            this.checkBox3.Font = new System.Drawing.Font("Yu Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkBox3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox3.Location = new System.Drawing.Point(361, 101);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(104, 24);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "x * x";
+            this.checkBox3.UseVisualStyleBackColor = false;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.RosyBrown;
+            this.textBox3.Location = new System.Drawing.Point(39, 142);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(461, 154);
+            this.textBox3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(551, 369);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Ex 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private TextBox textBox3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+using System.Text.RegularExpressions;
+
+namespace pr_1._2
+{
+    public partial class Form1 : Form
+    {
+        public int result;
+       
+        public Form1()
+        {
+            InitializeComponent();
+            textBox3.BackColor = textBox1.BackColor;  
+     
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string xstring = string.Join(string.Empty, Regex.Matches(textBox1.Text, @"\d+").OfType<Match>().Select(m => m.Value));
+            string bstring = string.Join(string.Empty, Regex.Matches(textBox2.Text, @"\d+").OfType<Match>().Select(m => m.Value));
+            double x = 0;
+            double b = 0;
+            double.TryParse(xstring, out x);
+            double.TryParse(bstring, out b);
+            double p = x * b;
+            double s = 1;
+            double f = 0;
+            if (checkBox1.Checked)
+            {
+                f = 2 * x;
+            }
+            if (checkBox2.Checked)
+            {
+                f = x + 4;
+            }
+            if (checkBox3.Checked)
+            {
+                f = x * x;
+            }
+            if (1 < p && p < 10)
+            {
+                s = Math.Pow(Math.Exp(x), f);
+            }
+            if (12 < p && p < 40) {
+                s = Math.Sqrt(Math.Abs(f) + 4 * b);
+            }
+            else
+            {
+                s = b * f * f;
+            }
+            textBox3.Text = $" при x = {x} \r\n при b = {b} \r\n s = {s} ";
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
